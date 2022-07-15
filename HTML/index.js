8d0115a5859b114b930abd7f9610855c8f6a6e2a
Jeannflo <78844952+Jeannflo@users.noreply.github.com>
2022-07-15 17:47:41 -0400

    Add files via upload

---
+++ HTML/index.js
@@ -0,0 +1,8 @@
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+//John is arrument to the greet function
+//name is aparameter of the greet function 
+greet('John', 'Smith');
+
+//Types of Function
