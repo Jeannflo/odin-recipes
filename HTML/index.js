8af639a78e0730f4080cd4d30e90934436994d66
Jeannflo <78844952+Jeannflo@users.noreply.github.com>
2022-07-29 15:02:56 -0400

    Delete HTML directory

--- HTML/index.js
+++
@@ -1,8 +0,0 @@
-function greet(name, lastName) {
-    console.log('Hello ' + name + ' ' + lastName);
-}
-//John is arrument to the greet function
-//name is aparameter of the greet function 
-greet('John', 'Smith');
-
-//Types of Function
